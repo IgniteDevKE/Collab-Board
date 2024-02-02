6eded3dadf36f72588f6112f4283462526c18f68
Ian Chege <en20101832020@student.mut.ac.ke>
2024-02-02 11:50:35 +0300

    feat: add auth loading state

--- providers/convex-client-provider.tsx
+++ providers/convex-client-provider.tsx
@@ -4,7 +4,7 @@ import { ClerkProvider, useAuth } from "@clerk/nextjs"
 import { ConvexProviderWithClerk } from "convex/react-clerk"
 import { AuthLoading, Authenticated, ConvexReactClient } from "convex/react"
 
-// import { Loading } from "@/components/auth/loading"
+import { Loading } from "@/components/auth/loading"
 
 interface ConvexClientProviderProps {
   children: React.ReactNode
@@ -21,9 +21,9 @@ export const ConvexClientProvider = ({
     <ClerkProvider>
       <ConvexProviderWithClerk useAuth={useAuth} client={convex}>
         <Authenticated>{children}</Authenticated>
-        {/* <AuthLoading> */}
-        {/* <Loading /> */}
-        {/* </AuthLoading> */}
+        <AuthLoading>
+          <Loading />
+        </AuthLoading>
       </ConvexProviderWithClerk>
     </ClerkProvider>
   )
