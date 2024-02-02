5c000e691eac2996a3f7d07a886dbf49a4e9eb44
Ian Chege <en20101832020@student.mut.ac.ke>
2024-02-02 11:24:01 +0300

    feat: Add Clerk.js and ConvexClientProvider to project

---
+++ convex/auth.config.js
@@ -0,0 +1,10 @@
+const authConfig = {
+    providers: [
+        {
+            domain: "https://funky-glowworm-73.clerk.accounts.dev",
+            applicationID: "convex",
+        },
+    ]
+};
+
+export default authConfig;
