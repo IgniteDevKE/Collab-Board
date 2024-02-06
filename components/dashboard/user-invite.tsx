7a7bd7d15713ed2639b474a3f24beec8f30990b9
Ian Chege <en20101832020@student.mut.ac.ke>
2024-02-06 16:03:52 +0300

    feat: navbar component

---
+++ components/dashboard/user-invite.tsx
@@ -0,0 +1,21 @@
+import { OrganizationProfile } from "@clerk/nextjs"
+import { Plus } from "lucide-react"
+
+import { Button } from "../ui/button"
+import { Dialog, DialogContent, DialogTrigger } from "../ui/dialog"
+
+export const UserInvite = () => {
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button variant="outline">
+          <Plus className="h-4 w-4 mr-2" />
+          Invite members
+        </Button>
+      </DialogTrigger>
+      <DialogContent className="p-0 bg-transparent border-none max-w-[880px]">
+        <OrganizationProfile />
+      </DialogContent>
+    </Dialog>
+  )
+}
