4160c6752ad88076302f4540334e45cd9c4abb60
Ian Chege <en20101832020@student.mut.ac.ke>
2024-02-08 21:53:25 +0300

    feat: Add BoardCard component to BoardList

--- components/dashboard/board-list.tsx
+++ components/dashboard/board-list.tsx
@@ -6,6 +6,7 @@ import { api } from "@/convex/_generated/api"
 import { EmptyBoards } from "./empty-boards"
 import { EmptyFavorites } from "./empty-favorites"
 import { EmptySearch } from "./empty-search"
+import { BoardCard } from "./board-card"
 
 interface BoardListProps {
   orgId: string
@@ -28,5 +29,26 @@ export const BoardList = ({ orgId, query }: BoardListProps) => {
   if (!data?.length) {
     return <EmptyBoards />
   }
-  return <div>{JSON.stringify(data)}</div>
+  return (
+    <div>
+      <h2 className="text-3xl">
+        {query.favorites ? "Favorite boards" : "Team boards"}
+      </h2>
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-5 mt-8 pb-10">
+        {data?.map((board) => (
+          <BoardCard
+            key={board._id}
+            id={board._id}
+            title={board.title}
+            imageUrl={board.imageUrl}
+            authorId={board.authorId}
+            authorName={board.authorName}
+            createdAt={board._creationTime}
+            orgId={board.orgId}
+            isFavorite={false}
+          />
+        ))}
+      </div>
+    </div>
+  )
 }
