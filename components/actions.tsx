294d6cd74222ca870bd8c59b814266ad0baf99e2
Ian Chege <en20101832020@student.mut.ac.ke>
2024-02-09 22:18:57 +0300

    feat: card actions init

---
+++ components/actions.tsx
@@ -0,0 +1,64 @@
+"use client"
+
+import { DropdownMenuContentProps } from "@radix-ui/react-dropdown-menu"
+
+import {
+  DropdownMenu,
+  DropdownMenuTrigger,
+  DropdownMenuContent,
+  DropdownMenuItem,
+} from "@/components/ui/dropdown-menu"
+
+interface ActionProps {
+  children: React.ReactNode
+  side?: DropdownMenuContentProps["side"]
+  sideOffset?: DropdownMenuContentProps["sideOffset"]
+  id: string
+  title: string
+}
+
+export const Actions = ({
+  children,
+  side,
+  sideOffset,
+  id,
+  title,
+}: ActionProps) => {
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>{children}</DropdownMenuTrigger>
+      {/* <DropdownMenuContent
+        onClick={(e) => e.stopPropagation()}
+        side={side}
+        sideOffset={sideOffset}
+        className="w-60"
+      >
+        <DropdownMenuItem onClick={onCopyLink} className="p-3 cursor-pointer">
+          <Link2 className="h-4 w-4 mr-2" />
+          Copy board link
+        </DropdownMenuItem>
+        <DropdownMenuItem
+          onClick={() => onOpen(id, title)}
+          className="p-3 cursor-pointer"
+        >
+          <Pencil className="h-4 w-4 mr-2" />
+          Rename
+        </DropdownMenuItem>
+        <ConfirmModal
+          header="Delete board?"
+          description="This will delete the board and all of its contents."
+          disabled={pending}
+          onConfirm={onDelete}
+        >
+          <Button
+            variant="ghost"
+            className="p-3 cursor-pointer text-sm w-full justify-start font-normal"
+          >
+            <Trash2 className="h-4 w-4 mr-2" />
+            Delete
+          </Button>
+        </ConfirmModal>
+      </DropdownMenuContent> */}
+    </DropdownMenu>
+  )
+}
--- components/dashboard/board-card/index.tsx
+++ components/dashboard/board-card/index.tsx
@@ -4,8 +4,10 @@ import { formatDistanceToNow } from "date-fns"
 import Image from "next/image"
 import Link from "next/link"
 import { useAuth } from "@clerk/nextjs"
+import { MoreHorizontal } from "lucide-react"
 
 import { Skeleton } from "@/components/ui/skeleton"
+import { Actions } from "@/components/actions"
 import { Overlay } from "./overlay"
 import { Footer } from "./footer"
 
@@ -39,6 +41,11 @@ export const BoardCard = ({
         <div className="relative flex-1 bg-stone-200">
           <Image src={imageUrl} alt="image" fill className="object-fit" />
           <Overlay />
+          <Actions id={id} title={title} side="right">
+            <button className="absolute top-1 right-1 opacity-0 group-hover:opacity-100 transition-opacity px-3 py-2 outline-none">
+              <MoreHorizontal className="text-white opacity-75 hover:opacity-100 transition-opacity" />
+            </button>
+          </Actions>
         </div>
         <Footer
           isFavorite={isFavorite}
