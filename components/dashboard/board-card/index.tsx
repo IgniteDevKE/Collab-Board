0f153ced900bbab0c6b954a31b99aa32c618bf67
Ian Chege <en20101832020@student.mut.ac.ke>
2024-02-13 11:15:24 +0300

    feat: Add favorite/unfavorite functionality to BoardCard component

--- components/dashboard/board-card/index.tsx
+++ components/dashboard/board-card/index.tsx
@@ -5,7 +5,10 @@ import Image from "next/image"
 import Link from "next/link"
 import { useAuth } from "@clerk/nextjs"
 import { MoreHorizontal } from "lucide-react"
+import { toast } from "sonner"
 
+import { useApiMutation } from "@/hooks/use-api-mutation"
+import { api } from "@/convex/_generated/api"
 import { Skeleton } from "@/components/ui/skeleton"
 import { Actions } from "@/components/actions"
 import { Overlay } from "./overlay"
@@ -35,6 +38,23 @@ export const BoardCard = ({
   const { userId } = useAuth()
   const authorLabel = userId === authorId ? "You" : authorName
   const createdAtLabel = formatDistanceToNow(createdAt, { addSuffix: true })
+
+  const { mutate: onFavorite, pending: pendingFavorite } = useApiMutation(
+    api.board.favorite
+  )
+  const { mutate: onUnfavorite, pending: pendingUnfavorite } = useApiMutation(
+    api.board.unfavorite
+  )
+
+  const toggleFavorite = () => {
+    if (isFavorite) {
+      onUnfavorite({ id }).catch(() =>
+        toast.error("Failed to unfavorite board")
+      )
+    } else {
+      onFavorite({ id, orgId }).catch(() => toast.error("Failed to favorite"))
+    }
+  }
   return (
     <Link href={`/board/${id}`}>
       <div className="group aspect-[100/127] border rounded-lg flex flex-col justify-between overflow-hidden">
@@ -52,8 +72,8 @@ export const BoardCard = ({
           title={title}
           authorLabel={authorLabel}
           createdAtLabel={createdAtLabel}
-          onClick={() => {}}
-          disabled={false}
+          onClick={toggleFavorite}
+          disabled={pendingFavorite || pendingUnfavorite}
         />
       </div>
     </Link>
