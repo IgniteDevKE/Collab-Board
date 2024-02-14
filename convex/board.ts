ea5488a252b582fbb381221f8e1b9e66f4b349f9
Ian Chege <en20101832020@student.mut.ac.ke>
2024-02-14 16:14:57 +0300

    feat: add room auth feature

--- convex/board.ts
+++ convex/board.ts
@@ -1,6 +1,6 @@
 import { v } from "convex/values"
 
-import { mutation } from "./_generated/server"
+import { mutation, query } from "./_generated/server"
 
 const images = [
   "/placeholder/1.svg",
@@ -155,6 +155,14 @@ export const unfavorite = mutation({
 
     await ctx.db.delete(existingFavorite._id)
 
+    return board
+  },
+})
+
+export const get = query({
+  args: { id: v.id("boards") },
+  handler: async (ctx, args) => {
+    const board = await ctx.db.get(args.id)
     return board
   },
 })
--- liveblocks.config.ts
+++ liveblocks.config.ts
@@ -2,8 +2,7 @@ import { createClient } from "@liveblocks/client"
 import { createRoomContext } from "@liveblocks/react"
 
 const client = createClient({
-  publicApiKey:
-    "pk_dev_H2p8Fd18SBXCID68aAcjAduy6K9iftTPh6evKKYqIKjdzso0cvKE5G3RIV19le4x",
+  authEndpoint: "/api/liveblocks-auth",
 })
 
 // Presence represents the properties that exist on every user in the Room
@@ -27,8 +26,11 @@ type Storage = {
 // provided by your own custom auth back end (if used). Useful for data that
 // will not change during a session, like a user's name or avatar.
 type UserMeta = {
-  // id?: string,  // Accessible through `user.id`
-  // info?: Json,  // Accessible through `user.info`
+  id?: string
+  info?: {
+    name?: string
+    picture?: string
+  }
 }
 
 // Optionally, the type of custom events broadcast and listened to in this
