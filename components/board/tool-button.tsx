1272b14436994d6fac6921ba80e59fcb189436e1
Ian Chege <en20101832020@student.mut.ac.ke>
2024-02-15 20:44:40 +0300

    fix: Refactor toolbar component with new icons and buttons

---
+++ components/board/tool-button.tsx
@@ -0,0 +1,35 @@
+"use client"
+
+import { LucideIcon } from "lucide-react"
+
+import { Hint } from "../hint"
+import { Button } from "../ui/button"
+
+interface IToolButtonProps {
+  label: string
+  icon: LucideIcon
+  onClick: () => void
+  isActive?: boolean
+  isDisabled?: boolean
+}
+
+export const ToolButton = ({
+  label,
+  icon: Icon,
+  onClick,
+  isActive,
+  isDisabled,
+}: IToolButtonProps) => {
+  return (
+    <Hint label={label} side="right" sideOffset={14}>
+      <Button
+        disabled={isDisabled}
+        onClick={onClick}
+        size="icon"
+        variant={isActive ? "boardActive" : "board"}
+      >
+        <Icon />
+      </Button>
+    </Hint>
+  )
+}
--- components/board/toolbar.tsx
+++ components/board/toolbar.tsx
@@ -1,18 +1,31 @@
+import {
+  Circle,
+  MousePointer2,
+  Pencil,
+  Redo2,
+  Square,
+  StickyNote,
+  Type,
+  Undo2,
+} from "lucide-react"
+
+import { ToolButton } from "./tool-button"
+
 export const Toolbar = () => {
   return (
     <div className="absolute top-[50%] -translate-y-[50%] left-2 flex flex-col items-center shadow-md gap-y-4">
       <div className="bg-white rounded-md p-1.5 flex gap-y-1 flex-col items-center shadow-md">
-        <div>Pencil</div>
-        <div>Eraser</div>
-        <div>Text</div>
-        <div>Shapes</div>
-        <div>Color</div>
-        <div>Size</div>
+        <ToolButton label="Select" icon={MousePointer2} onClick={() => {}} />
+        <ToolButton label="Text" icon={Type} onClick={() => {}} />
+        <ToolButton label="Sticky note" icon={StickyNote} onClick={() => {}} />
+        <ToolButton label="Rectangle" icon={Square} onClick={() => {}} />
+        <ToolButton label="Ellipse" icon={Circle} onClick={() => {}} />
+        <ToolButton label="Pen" icon={Pencil} onClick={() => {}} />
       </div>
 
       <div className="bg-white rounded-md p-1.5 flex gap-y-1 flex-col items-center shadow-md">
-        <div className="bg-white p-2 rounded-md shadow-md">Undo</div>
-        <div className="bg-white p-2 rounded-md shadow-md">Redo</div>
+        <ToolButton label="Undo" icon={Undo2} onClick={() => {}} />
+        <ToolButton label="Redo" icon={Redo2} onClick={() => {}} />
       </div>
     </div>
   )
