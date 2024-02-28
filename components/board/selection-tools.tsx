13a655de987a1040f97ac9cab46023106b0667ac
Ian Chege <en20101832020@student.mut.ac.ke>
2024-02-28 13:01:33 +0300

    feat: implement layer depth

--- components/board/selection-tools.tsx
+++ components/board/selection-tools.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import { memo } from "react"
-import { Trash2 } from "lucide-react"
+import { BringToFront, SendToBack, Trash2 } from "lucide-react"
 
 import { useMutation, useSelf } from "@/liveblocks.config"
 import { Camera, Color } from "@/types/canvas"
@@ -19,6 +19,48 @@ interface ISelectionToolsProps {
 export const SelectionTools = memo(
   ({ camera, setLastUsedColor }: ISelectionToolsProps) => {
     const selection = useSelf((me) => me.presence.selection)
+
+    const moveToFront = useMutation(
+      ({ storage }) => {
+        const liveLayersIds = storage.get("layersIds")
+        const indices: number[] = []
+        const arr = liveLayersIds.toImmutable()
+
+        for (let i = 0; i < arr.length; i++) {
+          if (selection.includes(arr[i])) {
+            indices.push(i)
+          }
+        }
+
+        for (let i = indices.length - 1; i >= 0; i--) {
+          liveLayersIds.move(
+            indices[i],
+            arr.length - 1 - (indices.length - 1 - i)
+          )
+        }
+      },
+      [selection]
+    )
+
+    const moveToBack = useMutation(
+      ({ storage }) => {
+        const liveLayerIds = storage.get("layersIds")
+        const indices: number[] = []
+
+        const arr = liveLayerIds.toImmutable()
+
+        for (let i = 0; i < arr.length; i++) {
+          if (selection.includes(arr[i])) {
+            indices.push(i)
+          }
+        }
+
+        for (let i = 0; i < indices.length; i++) {
+          liveLayerIds.move(indices[i], i)
+        }
+      },
+      [selection]
+    )
 
     const setFill = useMutation(
       ({ storage }, fill: Color) => {
@@ -53,13 +95,13 @@ export const SelectionTools = memo(
         <ColorPicker onChange={setFill} />
         <div className="flex flex-col gap-y-0.5">
           <Hint label="Bring to front">
-            <Button onClick={() => {}} variant="board" size="icon">
-              {/* <BringToFront /> */}
+            <Button onClick={moveToFront} variant="board" size="icon">
+              <BringToFront />
             </Button>
           </Hint>
           <Hint label="Send to back" side="bottom">
-            <Button onClick={() => {}} variant="board" size="icon">
-              {/* <SendToBack /> */}
+            <Button onClick={moveToBack} variant="board" size="icon">
+              <SendToBack />
             </Button>
           </Hint>
         </div>
