85d57054cf237045a181fdaff952d84ad17b2542
Ian Chege <en20101832020@student.mut.ac.ke>
2024-02-29 19:16:40 +0300

    fix: fix bug

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,12 @@
 import type { Metadata } from "next"
 import { Inter } from "next/font/google"
-import { ClerkProvider } from "@clerk/nextjs"
 
-import "./globals.css"
 import { ConvexClientProvider } from "@/providers/convex-client-provider"
 import { Toaster } from "@/components/ui/sonner"
 import { ModalProvider } from "@/providers/modal-provider"
 import { cn } from "@/lib/utils"
+
+import "./globals.css"
 
 const inter = Inter({ subsets: ["latin"] })
 
