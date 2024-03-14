3f383a1140f27334c2c04c08bc2d02e32e97c668
Ian Chege <en20101832020@student.mut.ac.ke>
2024-03-14 17:30:48 +0300

    fix: Update auth.config.js with string domain

--- convex/auth.config.js
+++ convex/auth.config.js
@@ -1,7 +1,7 @@
 const authConfig = {
   providers: [
     {
-      domain: process.env.CLERK_JWT_ISSUER_DOMAIN,
+      domain: "https://funky-glowworm-73.clerk.accounts.dev/",
       applicationID: "convex",
     },
   ],
