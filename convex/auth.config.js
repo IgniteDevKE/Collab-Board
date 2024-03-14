5d0ae8473f692e9f060c7895c7a071276eb18f1d
Ian Chege <en20101832020@student.mut.ac.ke>
2024-03-14 17:33:54 +0300

    fix: restore auth.config.js to use environment variable for domain

--- convex/auth.config.js
+++ convex/auth.config.js
@@ -1,7 +1,7 @@
 const authConfig = {
   providers: [
     {
-      domain: "https://funky-glowworm-73.clerk.accounts.dev/",
+      domain: process.env.CLERK_JWT_ISSUER_DOMAIN,
       applicationID: "convex",
     },
   ],
