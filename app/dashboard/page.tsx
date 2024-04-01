89da7aa75e91d930fc3eae2e0ef3b1088d050cb5
Ian Chege <en20101832020@student.mut.ac.ke>
2024-04-01 22:37:55 +0300

    fix: [bug]Refactor DashboardPage to use useSearchParams for search and favorites

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,25 +1,22 @@
 "use client"
 
 import { useOrganization } from "@clerk/nextjs"
+import { useSearchParams } from "next/navigation"
 
 import { EmptyOrg } from "@/components/dashboard/empty-org"
 import { BoardList } from "@/components/dashboard/board-list"
 
-interface DashboardPageProps {
-  searchParams: {
-    search?: string
-    favorites?: string
-  }
-}
-
-export default function DashboardPage({ searchParams }: DashboardPageProps) {
+export default function DashboardPage() {
   const { organization } = useOrganization()
+  const searchParams = useSearchParams()
+  const search = searchParams.get("search") || undefined
+  const favorites = searchParams.get("favorites") || undefined
   return (
     <div className="flex-1 h-[calc(100%-80px)] p-6">
       {!organization ? (
         <EmptyOrg />
       ) : (
-        <BoardList orgId={organization.id} query={searchParams} />
+        <BoardList orgId={organization.id} query={{ search, favorites }} />
       )}
     </div>
   )
