23254a8c8e6de4a5c1ac7014695904cf8567800a
Ian Chege <en20101832020@student.mut.ac.ke>
2024-04-14 19:43:38 +0300

    feat: validate schema on build

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,9 @@
+import createJiti from "jiti"
+import { fileURLToPath } from "node:url"
+const jiti = createJiti(fileURLToPath(import.meta.url))
+
+jiti("./lib/env")
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
