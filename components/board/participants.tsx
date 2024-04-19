4196e21bc80e6c8a76349181d27c1714d6f74c8d
Ian Chege <en20101832020@student.mut.ac.ke>
2024-04-19 22:09:52 +0300

    feat: Refactor Participants component to include Share workspace feature

--- components/board/participants.tsx
+++ components/board/participants.tsx
@@ -1,9 +1,11 @@
 "use client"
 
-import { useOthers, useSelf } from "@/liveblocks.config"
+import { Share2 } from "lucide-react"
 
-import { UserAvatar } from "./user-avatar"
 import { connectionIdToColor } from "@/lib/utils"
+import { useOthers, useSelf } from "@/liveblocks.config"
+import { Hint } from "../hint"
+import { UserAvatar } from "./user-avatar"
 
 const MAX_SHOWN_USERS = 3
 
@@ -14,7 +16,11 @@ export const Participants = () => {
 
   return (
     <div className="absolute h-12 top-2 right-2 bg-white rounded-md p-3 flex items-center shadow-md">
-      <div className="flex gap-x-2">
+      <div className="flex items-center gap-x-2">
+        <Hint label="Share workspace" side="bottom" sideOffset={18}>
+          <Share2 size={24} color="#000" />
+        </Hint>
+
         {currentUser && (
           <UserAvatar
             src={currentUser.info?.picture}
