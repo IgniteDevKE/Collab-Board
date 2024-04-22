f19740082c195b7b81d103d20c8132399fa80966
Ian Chege <en20101832020@student.mut.ac.ke>
2024-04-22 08:43:25 +0300

    fix: Refactor liveblocks-auth route; 1. error-handling(try-catch block) 2. constants for status codes

--- app/api/liveblocks-auth/route.ts
+++ app/api/liveblocks-auth/route.ts
@@ -5,6 +5,9 @@ import { ConvexHttpClient } from "convex/browser"
 import { api } from "@/convex/_generated/api"
 import { env } from "@/lib/env"
 
+const STATUS_UNAUTHORIZED = 403
+const STATUS_INTERNAL_SERVER_ERROR = 500
+
 const convex = new ConvexHttpClient(env.NEXT_PUBLIC_CONVEX_URL)
 
 const liveblocks = new Liveblocks({
@@ -12,31 +15,42 @@ const liveblocks = new Liveblocks({
 })
 
 export async function POST(request: Request) {
-  const authorization = await auth()
-  const user = await currentUser()
+  try {
+    const authorization = await auth()
+    const user = await currentUser()
 
-  if (!authorization || !user) {
-    return new Response("Unauthorized", { status: 403 })
+    if (!authorization || !user) {
+      return new Response("Unauthorized access", {
+        status: STATUS_UNAUTHORIZED,
+      })
+    }
+
+    const { room } = await request.json()
+    const board = await convex.query(api.board.get, { id: room })
+
+    if (board?.orgId !== authorization.orgId) {
+      return new Response("Unauthorized access", {
+        status: STATUS_UNAUTHORIZED,
+      })
+    }
+
+    const userInfo = {
+      name: user.firstName || "Teammate",
+      picture: user.imageUrl,
+    }
+
+    const session = liveblocks.prepareSession(user.id, { userInfo })
+
+    if (room) {
+      session.allow(room, session.FULL_ACCESS)
+    }
+
+    const { status, body } = await session.authorize()
+    return new Response(body, { status })
+  } catch (error) {
+    console.error(error)
+    return new Response("Internal server error", {
+      status: STATUS_INTERNAL_SERVER_ERROR,
+    })
   }
-
-  const { room } = await request.json()
-  const board = await convex.query(api.board.get, { id: room })
-
-  if (board?.orgId !== authorization.orgId) {
-    return new Response("Unauthorized", { status: 403 })
-  }
-
-  const userInfo = {
-    name: user.firstName || "Teammate",
-    picture: user.imageUrl,
-  }
-
-  const session = liveblocks.prepareSession(user.id, { userInfo })
-
-  if (room) {
-    session.allow(room, session.FULL_ACCESS)
-  }
-
-  const { status, body } = await session.authorize()
-  return new Response(body, { status })
 }
