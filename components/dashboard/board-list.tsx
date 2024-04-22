5bc1984811aec78fc927aa1c6831da73b590c473
Ian Chege <en20101832020@student.mut.ac.ke>
2024-04-22 10:10:03 +0300

    fix: Refactor boardId to workspaceId in BoardList component

--- components/dashboard/board-list.tsx
+++ components/dashboard/board-list.tsx
@@ -18,7 +18,7 @@ interface BoardListProps {
 }
 
 export const BoardList = ({ orgId, query }: BoardListProps) => {
-  const data = useQuery(api.boards.get, { orgId, ...query })
+  const data = useQuery(api.workspaces.get, { orgId, ...query })
   if (data === undefined)
     return (
       <div>
@@ -50,17 +50,17 @@ export const BoardList = ({ orgId, query }: BoardListProps) => {
         {query.favorites ? "Favorite workspace" : "Your workspace"}
       </h2>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-5 mt-8 pb-10">
-        {data?.map((board) => (
+        {data?.map((workspace) => (
           <BoardCard
-            key={board._id}
-            id={board._id}
-            title={board.title}
-            imageUrl={board.imageUrl}
-            authorId={board.authorId}
-            authorName={board.authorName}
-            createdAt={board._creationTime}
-            orgId={board.orgId}
-            isFavorite={board.isFavorite}
+            key={workspace._id}
+            id={workspace._id}
+            title={workspace.title}
+            imageUrl={workspace.imageUrl}
+            authorId={workspace.authorId}
+            authorName={workspace.authorName}
+            createdAt={workspace._creationTime}
+            orgId={workspace.orgId}
+            isFavorite={workspace.isFavorite}
           />
         ))}
       </div>
